4778208f8bce6d8962a76c7ea9b13ee2809f4f3b
27010-34-07 <27010-34-07@exchange.ad.afpanet>
2022-05-02 16:48:56 +0200

    02/05

--- exo quiz/script/index.js
+++ exo quiz/script/index.js
@@ -1,12 +1,75 @@
+let quiz = new Array();
+
 function ChargeInfosJson() {
-    /* Allons chercher un fichier JSON */
+    
     fetch('format_quiz.json')
-        .then(response => { /* Une fois que le fichier est chargé */
-            return response.json();  /* Convertissons le en json */
+        .then(response => { 
+            return response.json();
         })
-        .then(data => { /*Une fois le fichier converti*/
-            console.log(data); /*Appelons notre fonction */
-            CreateDivs(data);
+        .then(data => {
+            console.log(data);
+            CreatDivs(data);
         }
         );
 }
+
+
+//fonction qui affichent les questions
+function CreatDivs(data) {
+    let prev=document.getElementsByClassName("preview")[0];
+    for (let i = 0; i < data.length; i++) {
+            let quiz = document.createElement("div");
+            quiz.setAttribute("class","card");
+            
+            quiz.innerHTML =
+            '<h1 class="question">' + data[i].question + '</h1>'
+            + '<li class="choix">' + data[i].choices + '</li>'
+            prev.appendChild(quiz);
+        }
+    }
+
+
+    // pour le click du choix
+// evaluate[0].addEventListener("click", () => {
+//     if (selected == "true") {
+//         result[0].innerHTML = "True";
+//         result[0].style.color = "green";
+//     } else {
+//         result[0].innerHTML = "False";
+//         result[0].style.color = "red";
+//     }
+// })
+// }
+
+// if (start) {
+// iterate("0");
+// }
+
+// aide Guillaume : 
+//Affichage de la question et des réponse (manipulation du DOM)
+// function showQuestion(data) {
+//     dataElement.innerText = data.question;
+//     data.choices.forEach((answer) => {
+
+//         //on créé un bouton pour chaque réponse
+//         const button = document.createElement('button');
+//         button.innerText = answer;
+//         button.classList.add('btn');
+
+//         //pour la réponse correct , on rajoute un petit élement sur le bouton
+//         if (answer === question.correct) {
+//             ;
+//             button.dataset.correct = question.correct;
+//         }
+//     }
+
+//         //sur chaque bouton, on rajoute un evenement on click qui appelera la fonction selectAnswer
+//         button.addEventListener('click', LafonctionAAppelerLorsduClick);
+
+//         answerButtonsElement.appendChild(button);
+//     })
+//     //on cache la div explication
+//     explainElement.classList.add('hide');
+//     // on remplit cette div explication avec l'explication de la réponse de l'élement en cours du json
+//     explainElement.innerText = question.explanation;
+// }
