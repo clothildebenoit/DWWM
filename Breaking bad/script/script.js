ccca90b50ea4061b6c200957edd6f36f49ae8f60
27010-34-07 <27010-34-07@exchange.ad.afpanet>
2022-05-04 16:46:10 +0200

    04/05

---
+++ Breaking bad/script/script.js
@@ -0,0 +1,63 @@
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+
+fetch("https://breakingbadapi.com/api/characters", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+        createList(result);
+    })
+    .catch(error => console.log('error', error));
+
+
+
+// liste perso
+var createList = function (data) {
+    var perso = document.getElementById("preview");
+
+    for (i = 0; i < data.length; i++) {
+        var element = document.createElement('card');
+        element.setAttribute('class', 'carte')
+        console.log(data[i]);
+        var element2 = document.createElement('div');
+        element2.setAttribute('class', 'back')
+        element2.innerHTML = data[i].name;
+        var element3 = document.createElement('img');
+        element3.setAttribute('class', 'front')
+
+        element3.src = data[i].img;
+
+
+        element.appendChild(element2);
+        element.appendChild(element3);
+        perso.appendChild(element);
+    }
+}
+
+// function flip(event){
+//     let retourner = event.currentTarget;
+//     if (retourner.className === "carte"){
+//         if(retourner.style.transform == "rotateY(180deg)"){
+//             retourner.style.transform = "rotateY(0deg)";
+//         }
+//         else{
+//             retourner.style.transform = "rotateY(180deg)";
+//         }
+//     }
+// }
+// $('.carte').click(function(){
+//     $(this).toggleClass('flipped');
+//   });
+
+// const flips = document.getElementsByClassName("carte");
+
+
+// for (i = 0; i < flips.length; i++) {
+//     flips[i].addEventListener("click", flipcard);
+    
+// }
+// function flipcard() {
+//         flips[i].classList.toggle("flipcard");
+//     }
--- exo quiz/script/index.js
+++ exo quiz/script/index.js
@@ -1,32 +1,100 @@
 let quiz = new Array();
 
 function ChargeInfosJson() {
-    
+
     fetch('format_quiz.json')
-        .then(response => { 
+        .then(response => {
             return response.json();
         })
         .then(data => {
             console.log(data);
-            CreatDivs(data);
+            start(data);
         }
         );
 }
 
 
-//fonction qui affichent les questions
-function CreatDivs(data) {
-    let prev=document.getElementsByClassName("preview")[0];
-    for (let i = 0; i < data.length; i++) {
-            let quiz = document.createElement("div");
-            quiz.setAttribute("class","card");
-            
-            quiz.innerHTML =
-            '<h1 class="question">' + data[i].question + '</h1>'
-            + '<li class="choix">' + data[i].choices + '</li>'
-            prev.appendChild(quiz);
+// //fonction qui affichent les questions
+// function CreatDivs(data) {
+//     let prev=document.getElementsByClassName("preview")[0];
+//     for (let i = 0; i < data.length; i++) {
+//             let quiz = document.createElement("div");
+//             quiz.setAttribute("class","card");
+
+//             quiz.innerHTML =
+//             '<h1 class="question">' + data[i].question + '</h1>'
+//             + '<li class="choix">' + data[i].choices + '</li>'
+//             prev.appendChild(quiz);
+//         }
+//     }
+
+
+// fichier commun
+function CreateDivs(data) {
+    const question = document.getElementById("questionContainer");
+
+    for (let x = 0; x < data.length; x++) {
+
+        let questionListe = document.createElement("div");
+        let questionTitre = document.createElement("h1");
+
+        questionTitre.setAttribute("class", "titrequestion");
+        questionListe.setAttribute("id", "question" + x);
+
+        questionTitre.innerHTML += "<strong>Question : </strong>" + data[x].question;
+
+        questionListe.appendChild(questionTitre);
+
+        let repList = document.createElement("div");
+        repList.setAttribute("id", "reponseExplication" + x);
+        for (let y = 0; y < data[x].choices.length; y++) {
+            let propsListe = document.createElement("h2");
+
+            propsListe.setAttribute("id", "titreprops" + x + "" + y);
+            // propsListe.setAttribute("onclick", "resultat" + y + "()");
+            propsListe.innerHTML += data[x].choices[y];
+            questionListe.appendChild(propsListe);
+        }
+
+        repList.innerHTML = `<h3><strong>Réponse :</strong>  ${data[x].correct}</h3><p>${data[x].explanation}</p>`;
+
+        questionListe.appendChild(repList);
+        question.appendChild(questionListe);
+    }
+}
+
+var start = function (data) {
+    CreateDivs(data);
+
+    var round = 0
+
+    for (x = 0; x < data.length; x++) {
+        document.getElementById("question" + x).style.display = "none";
+    }
+
+    document.getElementById("question" + round).style.display = "grid";
+    document.getElementById("reponseExplication" + round).style.display = "none";
+
+    // document.getElementById("question" + round).style.gridTemplateColumns = "50% 50%";
+
+    document.getElementById("bouton").style.display = "none";
+    document.getElementById("titreprops" + round + "0").addEventListener("click", resultat0);
+
+
+    function resultat0() {
+        var stock = document.getElementById("titreprops" + round + "0");
+        if (stock.innerHTML == data[round].correct) {
+            console.log("yes")
+            document.getElementById("reponseExplication" + round).style.display = "block";
+            document.getElementById("bouton").style.display = "block";
         }
     }
+// Next button
+    document.getElementById("bouton").addEventListener("click", function(){
+       round = [round+1];
+    start(round);
+    });
+
 
 
     // pour le click du choix
@@ -41,11 +109,9 @@ function CreatDivs(data) {
 // })
 // }
 
-// if (start) {
-// iterate("0");
-// }
 
-// aide Guillaume : 
+
+// aide Guillaume :
 //Affichage de la question et des réponse (manipulation du DOM)
 // function showQuestion(data) {
 //     dataElement.innerText = data.question;
@@ -58,7 +124,6 @@ function CreatDivs(data) {
 
 //         //pour la réponse correct , on rajoute un petit élement sur le bouton
 //         if (answer === question.correct) {
-//             ;
 //             button.dataset.correct = question.correct;
 //         }
 //     }
