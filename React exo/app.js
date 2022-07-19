6f3eef7f2426e1d363f6a36e80d7eb9ea597b851
clothilde <coty@gmx.fr>
2022-07-19 12:53:45 +0200

    19/07

---
+++ React exo/app.js
@@ -0,0 +1,8 @@
+const title = React.createElement('h1', {}, 'Bonjour tout le monde')
+
+ReactDom.render(title, document.querySelector('#app'))
+
+// ReactDOM.render(
+//     <h1>Bonjour, monde !</h1>,
+//     document.getElementById('app')
+//   );
