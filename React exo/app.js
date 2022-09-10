8738aa3af57f4c4bc0ba1ed419d42b871dc3a981
clothilde <coty@gmx.fr>
2022-09-11 00:10:12 +0200

    11/09

--- React exo/app.js
+++ React exo/app.js
@@ -1,8 +1,17 @@
-const title = React.createElement('h1', {}, 'Bonjour tout le monde')
+// const title = React.createElement('h1', {}, 'Bonjour tout le monde')
 
-ReactDom.render(title, document.querySelector('#app'))
+// ReactDom.render(title, document.querySelector('#app'))
 
 // ReactDOM.render(
-//     <h1>Bonjour, monde !</h1>,
+//     <h1>Bonjour, tout le monde !</h1>,
 //     document.getElementById('app')
 //   );
+
+  const e = React.createElement;
+
+// Affiche un bouton « J’aime »
+return e(
+  'button',
+  { onClick: () => this.setState({ liked: true }) },
+  'J’aime'
+);
